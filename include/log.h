046e2da9e520be191cd523b1079f1844b980c369
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-11 20:48:03 -0400

    Add support to display number using log

--- include/log.h
+++ include/log.h
@@ -15,9 +15,24 @@
 						  uart_write_string("\033[0m \n\n")
 
 #define log_crtical(message) uart_write_string("\033[0;31m"); \
-						   	 LOG_MESSAGE_TEMPLATE("[Info]",message); \
+						   	 LOG_MESSAGE_TEMPLATE("[Critical]",message); \
 						   	 uart_write_string("\033[0m \n\n")
 
 #define log_debug(message) uart_write_string("\033[0;36m"); \
-						   	 LOG_MESSAGE_TEMPLATE("[Info]",message); \
+						   	 LOG_MESSAGE_TEMPLATE("[Debug]",message); \
 						   	 uart_write_string("\033[0m \n\n")
+
+#define log_info_number(message,number) uart_write_string("\033[0;32m"); \ 
+							     LOG_MESSAGE_TEMPLATE("[Info]",message); \
+							     uart_write_number(number); \
+							     uart_write_string("\033[0m \n\n")
+							     
+#define log_crtical_number(message,number) uart_write_string("\033[0;31m"); \ 
+							     LOG_MESSAGE_TEMPLATE("[Critical]",message); \
+							     uart_write_number(number); \
+							     uart_write_string("\033[0m \n\n")
+
+#define log_debug_number(message,number) uart_write_string("\033[0;36m"); \ 
+							     LOG_MESSAGE_TEMPLATE("[Debug]",message); \
+							     uart_write_number(number); \
+							     uart_write_string("\033[0m \n\n")
