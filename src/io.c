968db8a76dddb650e72796f14c3768154a73c179
IzaltinoDSouza <izaltinodsouza@gmail.com>
2023-06-12 21:22:05 -0400

    IO change to intel syntax inline assembly

--- src/io.c
+++ src/io.c
@@ -2,11 +2,11 @@
 
 void out8(u16 port,u8 data)
 {
-	__asm__ volatile("outb %0, %w1" : : "a"(data), "Nd"(port));
+	__asm__ volatile("outb %1,%0" : : "a"(data), "Nd"(port));
 }
 u8 in8(u16 port)
 {
 	u8 data;
-	__asm__ volatile("inb %w1, %0" : "=a"(data) : "Nd"(port));
+	__asm__ volatile("inb %0,%1" : "=a"(data) : "Nd"(port));
 	return data;
 }
